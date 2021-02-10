a93f9f0d605b541beaf3da9ffa6e0adfed8479dc
hibi <hibi@mail.jp>
2021-02-10 22:59:37 +0900

    1st commit

--- umaponly.py
+++ umaponly.py
@@ -21,40 +21,41 @@ from utils.customDataset import oct
 from network import resnet_face
 
 #疵種指定
-train_folders = 'oct_ind1' #folder2labelで既知クラス扱いにするフォルダ（train用）
-test_ind_folders = 'oct_ind1' #既知クラス扱いにするフォルダ（テスト用）
-test_ood_folders = 'oct_ood1' #未知クラス扱いにするフォルダ（テスト用）
+train_folders = 'oct_ind1few10' #folder2labelで既知クラス扱いにするフォルダ（train用）
+test_ind_folders = 'oct_temp1' #既知クラス扱いにするフォルダ（テスト用）
+test_ood_folders = 'oct_temp2' #未知クラス扱いにするフォルダ（テスト用）
 
 #使用データ
 #dir_train = '/home/keisoku/work/ood2/data/oct/minidata'
 #dir_test = dir_train
 dir_train = '/home/keisoku/work/ood2/data/oct/train'
-dir_test = '/home/keisoku/work/ood2/data/oct/test'
+#dir_test = '/home/keisoku/work/ood2/data/oct/test'
+dir_test = '/home/keisoku/work/ood2/data/oct/temp/'
 
 #学習済みモデル
-trainedmodel = "/home/keisoku/work/ood_face/src/softmax_numfeature2/CP_best2.pth"
+trainedmodel = "/home/keisoku/work/ood_face/out/20210210途中経過/out20210210/softmax/CP_best.pth"
 backbone = 'resnet18'
-metric = 'softmax'
+metric = 'sortmax'
 svalue = None #autoならNone,固定値ならその値を入れる
 pretrain = True #pretrainありならTrueにする
-fsize = 2 #512 #抽出する特徴量のサイズ
+fsize = 512 #512 #抽出する特徴量のサイズ
 in_channels = 3 #入力チャンネル数
-numclass = 3 #既知クラスの個数
+numclass = 4 #既知クラスの個数
 
 #保存場所。モデルで判定したnpyもここに保存（２回以降の実行ではモデル判定をスキップできる）.モデルか使用データを変えた場合はnpyを消すこと。
-savedir = os.path.join(os.path.dirname(trainedmodel), 'umap2')
+savedir = os.path.join(os.path.dirname(trainedmodel), 'umap')
 
 #---UMAP用パラメータ----
 n_neighbors = 200 #15
 min_dist = 0.0
 n_components = 2 #何次元に変換するか
-is_supervised = True #正解ラベルを使用するならTrue #時間かかるので注意
+is_supervised = False #正解ラベルを使用するならTrue #時間かかるので注意
 random_seed = 42
 low_memory = False #メモリ節約するか
-prune_rate = 0.2 #UMAPを作るときに使用するtrain dataの割合（間引き率）
+prune_rate = 0.1 #UMAPを作るときに使用するtrain dataの割合（間引き率）
 
 #グラフ
-is_dispeach = True #学習データ、テストデータのUMAP画像を単体で出力するならTrue
+is_dispeach = False #学習データ、テストデータのUMAP画像を単体で出力するならTrue
 #指定可能な色一覧：https://pythondatascience.plavox.info/matplotlib/%E8%89%B2%E3%81%AE%E5%90%8D%E5%89%8D
 if numclass == 3:
     cmap = ListedColormap(['blue','green',  'red', 'black'])
@@ -64,7 +65,7 @@ if numclass ==4:
     cmap_mild = ListedColormap(['lightblue','lightgreen',  'lightsalmon', 'violet', 'black'])
 
 
-def makeumap(f, label, w=None):
+def makeumapfitter(f, label):
     mapper = umap.UMAP(
         n_neighbors = n_neighbors,
         min_dist = min_dist,
@@ -75,12 +76,9 @@ def makeumap(f, label, w=None):
         )
     fitter = mapper.fit(f, y=label) if is_supervised else mapper.fit(f)
     f_embeded  = fitter.embedding_
-    w_embeded = fitter.transform(w) if w is not None else None
     
-    if w is None:
-        return fitter, f_embeded
-    else:
-        return fitter, f_embeded, w_embeded
+    return fitter, f_embeded
+
 
 def pred(device, net, fsize, loader, n_data, prune_rate=None):
     net.eval()
@@ -151,7 +149,7 @@ if __name__ == '__main__':
     
     if n_components != fsize:
         logging.info(f'UMAPing for train data...')
-        fitter, train_f_embeded = makeumap(train_f, train_label)
+        fitter, train_f_embeded = makeumapfitter(train_f, train_label)
         logging.info(f'UMAPing done.')
     else:
         train_f_embeded = train_f
@@ -200,50 +198,6 @@ if __name__ == '__main__':
     xmin = np.min([np.min(train_f_embeded[:,0]),np.min(test_ind_f_embeded[:,0]),np.min(test_ood_f_embeded[:,0])]) - 5
     ymax = np.max([np.max(train_f_embeded[:,1]),np.max(test_ind_f_embeded[:,1]),np.max(test_ood_f_embeded[:,1])]) + 5
     ymin = np.min([np.min(train_f_embeded[:,1]),np.min(test_ind_f_embeded[:,1]),np.min(test_ood_f_embeded[:,1])]) - 5
-
-    #それぞれ独立の描写
-    if is_dispeach:
-        #train
-        fig, ax = plt.subplots(1, figsize=(8.0, 6.0))
-        plt.scatter(train_f_embeded[:,0], train_f_embeded[:,1], s=0.3, c=train_label, cmap=cmap_mild, vmin=-0.5, vmax=numclass+1-0.5, alpha=0.01)
-        #plt.scatter(w_embeded[:,0], w_embeded[:,1], s=30.0, c=np.arange(numclass), marker='*', cmap=cmap, vmin=-0.5, vmax=numclass+1-0.5, alpha=1.0)
-        plt.xlim(xmin, xmax)
-        plt.ylim(ymin, ymax)
-        #plt.setp(ax, xticks=[], yticks=[])
-        cbar = plt.colorbar(boundaries=np.arange(numclass+2)-0.5)
-        cbar.set_ticks(np.arange(numclass+1))
-        legend = ['class' + str(i) for i in range(numclass)]
-        legend.append('Unknown')
-        cbar.set_ticklabels(legend)
-        plt.title('Train Data Embedded via UMAP')
-        fig.savefig(os.path.join(savedir, "out_train.png"), format='png', dpi=600)
-        #test IND
-        fig, ax = plt.subplots(1, figsize=(8.0, 6.0))
-        plt.scatter(test_ind_f_embeded[:,0], test_ind_f_embeded[:,1], s=0.3, c=test_ind_label, cmap=cmap, vmin=-0.5, vmax=numclass+1-0.5, alpha=1.0)
-        plt.xlim(xmin, xmax)
-        plt.ylim(ymin, ymax)
-        #plt.setp(ax, xticks=[], yticks=[])
-        cbar = plt.colorbar(boundaries=np.arange(numclass+2)-0.5)
-        cbar.set_ticks(np.arange(numclass+1))
-        legend = ['class' + str(i) for i in range(numclass)]
-        legend.append('Unknown')
-        cbar.set_ticklabels(legend)
-        plt.title('Test [Known] Data Embedded via UMAP')
-        fig.savefig(os.path.join(savedir, "out_test_ind.png"), format='png', dpi=600)
-        #test OOD
-        fig, ax = plt.subplots(1, figsize=(8.0, 6.0))
-        plt.scatter(test_ood_f_embeded[:,0], test_ood_f_embeded[:,1], s=3.0, c=np.ones(n_test_ood, dtype=np.int)*numclass, marker='x', cmap=cmap, vmin=-0.5, vmax=numclass+1-0.5, alpha=1.0)
-        plt.xlim(xmin, xmax)
-        plt.ylim(ymin, ymax)
-        #plt.setp(ax, xticks=[], yticks=[])
-        cbar = plt.colorbar(boundaries=np.arange(numclass+2)-0.5)
-        cbar.set_ticks(np.arange(numclass+1))
-        legend = ['class' + str(i) for i in range(numclass)]
-        legend.append('Unknown')
-        cbar.set_ticklabels(legend)
-        plt.title('Test [UNknown] Data Embedded via UMAP')
-        fig.savefig(os.path.join(savedir, "out_test_ood.png"), format='png', dpi=600)
-
 
     #学習データと既知を1枚に統合
     fig, ax = plt.subplots(1, figsize=(8.0, 6.0))
